4f97980d3469d3b0bed0be202adc4da3c7f6ad3d
AxerTheAxe <AxerTheAxe@proton.me>
2023-11-26 15:28:38 -0700

    Add color pallete.

---
+++ lua/havona/palette.lua
@@ -0,0 +1,17 @@
+return {
+  base = "#141122",
+  surface = "#262041",
+  overlay = "#494776",
+  muted = "#535188",
+  subtle = "#8f83b8",
+  text = "#e0def4",
+  azalea = "#ff83af",
+  gold = "#f8b15d",
+  havona = "#8b84ee",
+  spearmint = "#00a981",
+  puya = "#00c5d7",
+  lilac = "#d599f8",
+  hi_low = "#1e1937",
+  hi_med = "#292442",
+  hi_high = "#2b2848"
+}
